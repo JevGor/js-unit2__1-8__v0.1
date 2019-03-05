e7f7f581c06aa4e23dad37b1393027ecb728349e
JevGor <reawqk@mail.ru>
2019-03-06 00:21:58 +0100

    Update js__u2__t3.js

--- js__u2__t3.js
+++ js__u2__t3.js
@@ -2,12 +2,29 @@
 // Значения переменных X, Y, Z поменять местами так, чтобы они оказались 
 // упорядоченными по возрастанию
 
+
 var x, y, z;
-x = 15, y = 10, z = 5;
+x = 4, y = 1, z = 6;
+var sum = x+y+z;
 
-    x = x+y+z; // 30
-    z = x-y-z; // 15 
-    x = x-y-z; // 5
-    y = z-x; // 10
+if ((x > y) && (x > z) && (y > z)) {
+    console.log(x, y, z);
+    
+} else if ((x > y) && (x > z) && (z > y)) {
+    console.log(x, z = y, (y = sum - x - y));
 
-console.log(x, y, z);
+} else if ((y > x) && (y > z) && (x > z)) {
+    console.log(y = x, (x = sum - y - z) , z);
+
+} else if ((y > x) && (y > z) && (z > x)) {
+    console.log(y = x , z, (x = sum - y - z));
+
+} else if ((z > x) && (z > y) && (x > y)) {
+    console.log(z = x, (x = (sum-z-y)), y);
+
+} else if ((z > x) && (z > y) && (y > x)) {
+    console.log(z = x, (y = sum - x - z), x);
+
+} else {
+    console.log('Не x, не у и не z')
+};
